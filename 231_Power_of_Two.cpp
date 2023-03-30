e65a0438d1ca604031572f9b557502b6e1881229
Ayush Sachan <97084090+ayushsachan123@users.noreply.github.com>
2023-03-30 10:59:44 +0530

    Create 231_Power_of_Two.cpp

---
+++ 231_Power_of_Two.cpp
@@ -0,0 +1,15 @@
+//https://leetcode.com/problems/power-of-two/
+
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        if(n<1){
+            return false;
+        }
+        if(n==1){
+            return true;
+        }
+        return ((n%2==0) && isPowerOfTwo(n/2));
+    }
+};
+
